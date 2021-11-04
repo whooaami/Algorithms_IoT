aafd7d1bf6130760ba962297f727cf033e5eae84
yark0 <86779145+whooaami@users.noreply.github.com>
2021-11-04 19:10:17 +0200

    Update unittest (add 3 functions)

--- Lab_4/test_bfs.py
+++ Lab_4/test_bfs.py
@@ -1,11 +1,11 @@
 import unittest
-from bfs import graph
+from bfs import bfs
 
 
-class TestGraph(unittest.TestCase):
+class BFSTest(unittest.TestCase):
 
-    def test_graph(self):
-        expected_graph = {
+    def test_positive_result(self):
+        graph = {
             'A': ['B', 'C'],
             'B': ['D', 'E'],
             'C': ['F', 'G'],
@@ -17,7 +17,37 @@ class TestGraph(unittest.TestCase):
             'I': []
         }
 
-        self.assertEqual(expected_graph, graph)
+        self.assertEqual(bfs(graph, 'A'), list(['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I']))
+
+    def test_number_result(self):
+        graph_numbers = {
+            '0': ['1', '2'],
+            '1': ['3', '4'],
+            '2': ['5', '6'],
+            '3': ['7', '8'],
+            '4': [],
+            '5': [],
+            '6': [],
+            '7': [],
+            '8': []
+        }
+
+        self.assertEqual(bfs(graph_numbers, '0'), list(['0', '1', '2', '3', '4', '5', '6', '7', '8']))
+
+    def test_wrong_result(self):
+        wrong_graph = {
+            'A': ['C', 'B'],
+            'B': ['E', 'D'],
+            'C': ['F', 'G'],
+            'D': ['H', 'I'],
+            'E': [],
+            'F': [],
+            'G': [],
+            'H': [],
+            'I': []
+        }
+
+        self.assertNotEqual(bfs(wrong_graph, 'A'), list(['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I']))
 
 
 if __name__ == '__main__':
